995b9886bf31f6d59f12776bd36eff19b65b0651
Arunava Nandi <100985802+ArunavaNandi99@users.noreply.github.com>
2023-04-09 17:36:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,90 +1,59 @@
-const express = require('express')
-let { MongoClient, ObjectId } = require('mongodb')
-let sanitizeHTML = require('sanitize-html')
 
-
-const app = express()
-let db
-let port = process.env.port
-if(port == null || port == ""){
-  port = 3000
+const itemTemplate = (item) => {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id=${item._id} class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id=${item._id} class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
 }
 
-app.use(express.static('public'))
-const go = async (client) => {
-  try {
-    client = new MongoClient('mongodb+srv://admin:arunava@cluster0.r498ndk.mongodb.net/TodoApp1?retryWrites=true&w=majority');
-    await client.connect();
-    db = client.db();
-    app.listen(port);
-  } catch (error) {
-    console.error(error);
-  }
-};
-go()
+//Initial Page Load Render
+let ourHTML = items.map((item) => {
+    return itemTemplate(item)
+}).join("")
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-const passwordProtected = (req, res, next) => {
-  res.set('WWW-Authenticate', 'Basic realm="Simple Todo App"')
-  if (req.headers.authorization == "Basic YXJ1bmF2YTphcnVuYXZh") {
-    next()
-  } else {
-    res.status(401).send("Authentication required")
-  }
-
-}
-app.use(passwordProtected)
-
-app.get('/', async (req, res) => {
-  const items = await db.collection('items').find().toArray()
-  res.send(`<!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Simple To-Do App</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container">
-        <h1 class="display-4 text-center py-1">To-Do App</h1>
-        
-        <div class="jumbotron p-3 shadow-sm">
-          <form id="create-form" action="/create-item" method="POST">
-            <div class="d-flex align-items-center">
-              <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-              <button class="btn btn-primary">Add New Item</button>
-            </div>
-          </form>
-        </div>
-        <ul id="item-list" class="list-group pb-5">
-        
-        </ul>   
-      </div> 
-      <script>
-      let items = ${JSON.stringify(items)}
-      </script>
-      <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
-      <script src="/browser.js"></script>
-    </body>
-    </html>`)
+//create Feature
+let createFiled = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    axios.post('/create-item', { text: createFiled.value })
+        .then((response) => {
+            document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+            createFiled.value = ""
+            createFiled.focus();
+        }).catch(() => {
+            console.log("Please try again later.");
+        })
 })
 
-app.post('/create-item', async (req, res) => {
-  let safeText = sanitizeHTML(req.body.text , {allowedTags: [] , allowedAttributes : {}})
-  const info = await db.collection("items").insertOne({ text: safeText })
-  res.json({ _id: info.insertedId, text: safeText })
-})
 
-app.post('/update-item', async (req, res) => {
-  let safeText = sanitizeHTML(req.body.text , {allowedTags: [] , allowedAttributes : {}})
-  await db.collection('items').findOneAndUpdate({ _id: new ObjectId(req.body.id) }, { $set: { text: safeText} })
-  res.send("Success")
-})
-app.post('/delete-item', async (req, res) => {
-  await db.collection('items').deleteOne({ _id: new ObjectId(req.body.id) })
-  res.send("Success")
-})
+document.addEventListener("click", (e) => {
+    //Delete Feature
+    if (e.target.classList.contains('delete-me')) {
+        if (confirm("Do you really want to delete this item permanently")) {
+            axios.post('/delete-item', { id: e.target.getAttribute("data-id") })
+                .then(() => {
+                    e.target.parentElement.parentElement.remove()
+                }).catch(() => {
+                    console.log("Please try again later.");
+                })
+        }
+    }
+
+    //Update Feature
+    if (e.target.classList.contains('edit-me')) {
+        let userInput = prompt("Enter your desire new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+        if (userInput) {
+            axios.post('/update-item', { text: userInput, id: e.target.getAttribute("data-id") })
+                .then(() => {
+                    e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+                }).catch(() => {
+                    console.log("Please try again later.");
+                })
+        }
+    }
+}) 
 
